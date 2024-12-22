5a54608cdde09cbc114f355d3078ee0c31c48b10
Weverton de Castro Blanco <129322041+Weverton2005Castro@users.noreply.github.com>
2024-12-22 17:33:04 +0000

    ajustei tudo

---
+++ scripts/BoasVindas.js
@@ -0,0 +1,6 @@
+
+var nome = prompt("qual seu nome?");
+
+alert(`Olá, ${nome}! Seja muito Bem Vindo, é um prazer ter você aqui.`);
+
+window.location.href = "../Pages/index.html";
---
+++ scripts/page5.js
@@ -0,0 +1,4 @@
+function show_message() {
+    var message = document.getElementById("message");
+    message.style.display = "block";
+}
